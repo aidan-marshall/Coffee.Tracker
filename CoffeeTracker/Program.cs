e60d2d40bf584dc3c4d7ebd89ce30cb67f3f6f03
aidan-marshall <aidanpm.marshall@gmail.com>
2024-08-25 21:16:14 +0200

    Updated insert method to POST request

--- CoffeeTracker/Program.cs
+++ CoffeeTracker/Program.cs
@@ -10,15 +10,7 @@ app.MapGet("/", () => {
     return Results.Ok("Hello, World!");
 });
 
-app.MapGet("/coffee/create", () => {
-
-    var coffeeRecord = new CoffeeRecord
-    {
-        UserId = 123,
-        TimeOfConsumption = DateTime.Now,
-        CoffeeType = "Espresso",
-        Location = "Office"
-    };
+app.MapPost("/coffee/create", (CoffeeRecord coffeeRecord) => {
     
     var connection = new NpgsqlConnection(connectionString: "Host=localhost;Port=5432;Username=postgres;Password=random;Database=CoffeeTracker;");
     var coffeRecordRepository = new CoffeeRecordRepository(connection);
@@ -27,6 +19,7 @@ app.MapGet("/coffee/create", () => {
     var recordId = coffeRecordService.CreateCoffeRecord(coffeeRecord);
 
     return Results.Ok(recordId);
+
     });
 
 app.Run();
