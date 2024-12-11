84095d9511b2bcffd1182b1f112d6b1bdba7d484
Aidan Marshall <aidan.marshall@structureit.net>
2024-12-11 23:12:56 +0200

    Setup Migrations with EF Core.

---
+++ CoffeeTracker/Migrations/20241211203612_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CoffeeTracker.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "records",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    user_id = table.Column<int>(type: "integer", nullable: false),
+                    time_of_consumption = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    coffee_type = table.Column<string>(type: "text", nullable: false),
+                    location = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_records", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "records");
+        }
+    }
+}
---
+++ CoffeeTracker/Migrations/CoffeeTrackerContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace CoffeeTracker.Migrations;
+
+public class CoffeeTrackerContextFactory : IDesignTimeDbContextFactory<CoffeeTrackerContext>
+{
+    public CoffeeTrackerContext CreateDbContext(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Development.json")
+            .Build();
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        
+        var optionsBuilder = new DbContextOptionsBuilder<CoffeeTrackerContext>();
+        optionsBuilder.UseNpgsql(connectionString);
+
+        return new CoffeeTrackerContext(optionsBuilder.Options);
+    }
+}
